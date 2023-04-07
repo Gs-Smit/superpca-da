f222210faa8c34fd1d5cac6dc68f59504008ea81
Gs-Smit <77085588+Gs-Smit@users.noreply.github.com>
2023-04-07 14:03:19 +0800

    Delete getlabels directory

--- getlabels/common/fea_norm.m
+++
@@ -1,6 +0,0 @@
-function fea = fea_norm(fea);
-
-[nSmp,nFea] = size(fea);
-for i = 1:nSmp
-     fea(i,:) = fea(i,:) ./ max(1e-12,norm(fea(i,:)));
-end
--- getlabels/common/getlabel.m
+++
@@ -1,6 +0,0 @@
-function label=getlabel(classSize)
-
-label = [];
-for i=1:length(classSize)
-    label = [label i*ones(1,classSize(i))];
-end
